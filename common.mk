51d4cf9bc62d119fa5622afc021b761c0efdd087
Balazs Fekete <bfekete@ccny.cuny.edu>
2014-08-18 21:56:00 -0400

    Fixing -pthread flag for Mac OSX.

--- common.mk
+++ common.mk
@@ -3,7 +3,18 @@ ifndef GHAASDIR
 export GHAASDIR=/usr/local/share/ghaas
 endif
 
+ifeq ($(UNIX),Darwin)
+ifndef ($(CUSTOM_INC))
+	CUSTOM_INC=-I/sw/include
+endif
+ifndef ($(CUSTOM_LIB))
+	CUSTOM_LIB=-L/sw/lib
+endif
+else
+	PTHREAD_OPS=-pthread
+endif
+
 export UNIXCC=gcc
-export UNIXCCOPS=-g -Wall -pthread -fsigned-char -D_GNU_SOURCE
-export UNIXLIBS=-pthread -lm
+export UNIXCCOPS=-g -Wall -fsigned-char -D_GNU_SOURCE
+export UNIXLIBS=$(CUSTOM_LIB) $(PTHREAD_OPS) -lm
 export UNIXMAKE=make
